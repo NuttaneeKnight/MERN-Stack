dfac1074a3db3e8d5b6b897fd518177dcfd33fc8
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-03-14 17:18:24 -0400

    installed dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 
 //express
 const app = express();
+
+//routes
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the app" });
+});
+
+//listen for request
+app.listen(process.env.PORT, () => {
+  console.log("listening on port", process.env.PORT);
+});
