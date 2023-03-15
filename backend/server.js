c2371f933827b3df539814e6684ea751cc413504
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-03-15 11:42:34 -0400

    set up mongoose

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,16 @@ app.use((req, res, next) => {
 //routes
 app.use("/api/workouts", workoutRoutes);
 
-//listen for request
-app.listen(process.env.PORT, () => {
-  console.log("listening on port", process.env.PORT);
-});
+// connec to db(mongoDB)
+// async waiting for a promise
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen for request after it's connected to the db
+    app.listen(process.env.PORT, () => {
+      console.log("listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
