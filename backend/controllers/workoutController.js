e6bcd24da62ada002617ad07e931df5b832ca42c
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-03-16 15:03:12 -0400

    created delete workout controller

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -38,11 +38,27 @@ const createWorkout = async (req, res) => {
   }
 };
 // DELETE a workout
+const deleteWorkout = async (req, res) => {
+  const { id } = req.params;
 
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    // had to type id or the app will crash
+    return res.status(404).json({ error: "No such workout" });
+  }
+
+  const workout = await Workout.findOneAndDelete({ _id: id }); //property for mongodb to find id
+
+  if (!workout) {
+    return res.status(400).json({ error: "No such workout" });
+  }
+
+  res.status(200).json(workout);
+};
 // UPDATE a workout
 
 module.exports = {
   getWorkouts,
   getWorkout,
   createWorkout,
+  deleteWorkout,
 };
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -3,6 +3,7 @@ const {
   createWorkout,
   getWorkouts,
   getWorkout,
+  deleteWorkout,
 } = require("../controllers/workoutController");
 
 const router = express.Router();
@@ -17,11 +18,7 @@ router.get("/:id", getWorkout);
 router.post("/", createWorkout);
 
 // DELETE a workout
-router.delete("/:id", (req, res) => {
-  res.json({
-    message: "DELETE a new workout",
-  });
-});
+router.delete("/:id", deleteWorkout);
 
 // UPDATE a workout
 router.patch("/:id", (req, res) => {
