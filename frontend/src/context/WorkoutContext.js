283a0cdeb963e21bef5388017722e4536afc7f71
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-04-03 17:07:10 -0400

    created WorkoutContext.Provider

---
+++ frontend/src/context/WorkoutContext.js
@@ -0,0 +1,11 @@
+import { createContext } from "react";
+
+export const WorkoutsContext = createContext()
+
+const WorkoutsContextProvider = () => {
+  return (
+    <WorkoutsContext.Provider>
+      
+    </WorkoutsContext.Provider>
+  )
+}
