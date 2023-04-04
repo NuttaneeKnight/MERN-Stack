a5168a082cfd109f7eeaa1c9abbc36dfd749df9c
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-04-04 17:53:34 -0400

    passed children in workoutcontext

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -1,11 +1,7 @@
 import { createContext } from "react";
 
-export const WorkoutsContext = createContext()
+export const WorkoutsContext = createContext();
 
 const WorkoutsContextProvider = ({ children }) => {
-  return (
-    <WorkoutsContext.Provider>
-      
-    </WorkoutsContext.Provider>
-  )
-}
+  return <WorkoutsContext.Provider>{children}</WorkoutsContext.Provider>;
+};
