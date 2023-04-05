65fedc5f094e9a3d72880df58a60c8ffb4ccda66
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-04-04 20:09:49 -0400

    set workoutReducer func

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -3,17 +3,28 @@ import { createContext, useReducer } from "react";
 export const WorkoutsContext = createContext();
 
 export const workoutsReducer = (state, action) => {
-  
-}
+  switch (action.type) {
+    case "SET_WORKOUTS":
+      return {
+        workouts: action.payload,
+      };
+    case "CREATE_WORKOUT":
+      return {
+        workouts: [action.payload, ...state.workouts],
+      };
+    default:
+      return state;
+  }
+};
 
 export const WorkoutsContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(workoutsReducer, {
-    workouts: null
-  })
+    workouts: null,
+  });
 
   return (
-    <WorkoutsContext.Provider value={{workouts: []}}>
+    <WorkoutsContext.Provider value={{ workouts: [] }}>
       {children}
     </WorkoutsContext.Provider>
-    )
+  );
 };
